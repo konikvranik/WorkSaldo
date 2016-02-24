cc3e9879c42e246328a5599586b5ff495188be68
hPa <petr@vranik.name>
2016-02-24 07:52:11 +0100

    removed deprecated common

--- common/src/main/java/net/suteren/worksaldo/adp/Base64.java
+++
@@ -1,8 +0,0 @@
-package net.suteren.worksaldo.adp;
-
-/**
- * Created by petr on 23.02.2016.
- */
-public interface  Base64 {
-     byte[] encode(byte[] bytes);
-}
--- common/src/main/java/net/suteren/worksaldo/model/Task.java
+++
@@ -1,7 +0,0 @@
-package net.suteren.worksaldo.model;
-
-/**
- * Created by petr on 23.02.2016.
- */
-public class Task {
-}
