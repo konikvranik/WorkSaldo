42c6dbffc6eba00b9abbd197f459bebea6f61002
Petr Vranik <builds@travis-ci.org>
2016-03-18 19:06:58 +0100

    bintray deploy

--- scripts/pubreports.sh
+++ scripts/pubreports.sh
@@ -2,12 +2,12 @@
 cd $HOME
 git clone -b gh-pages "https://$GITHUB_TOKEN@github.com/konikvranik/worksaldo.git" gh-pages || exit 1
 echo Copying lint
-cp -r $TRAVIS_BUILD_DIR/android/build/outputs/lint-results* gh-pages
+cp -ru $TRAVIS_BUILD_DIR/android/build/outputs/lint-results* gh-pages
 STAT=$?
 echo Copying report
-cp -r $TRAVIS_BUILD_DIR/android/build/outputs/report* gh-pages
+cp -ru $TRAVIS_BUILD_DIR/android/build/outputs/report* gh-pages
 [ $? -eq 0 ] && STAT=0
-cp -r $TRAVIS_BUILD_DIR/android/build/report* gh-pages
+cp -ru $TRAVIS_BUILD_DIR/android/build/report* gh-pages
 [ $? -gt 0 -a $STAT -gt 0 ] && exit 1
 echo CD to gh-pages
 cd $HOME/gh-pages || exit 1
