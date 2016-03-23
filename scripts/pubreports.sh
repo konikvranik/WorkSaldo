277c1d41b463751432112dd1fcc5a8f5cc836058
Petr Vranik <petr.vranik@lmc.eu>
2016-03-23 16:54:54 +0100

    changelog update by travis

--- scripts/pubreports.sh
+++ scripts/pubreports.sh
@@ -12,10 +12,12 @@ cp -ru $TRAVIS_BUILD_DIR/android/build/report* gh-pages
 echo CD to gh-pages
 cd $HOME/gh-pages || exit 1
 echo Adding lint
+cp -ru $TRAVIS_BUILD_DIR/android/CHANGELOG.html gh-pages
 git add lint*
 STAT=$?
 echo Adding report
 git add report*
+git add CHANGELOG.html
 [ $? -gt 0 -a $STAT -gt 0 ] && exit 1
 echo Commiting
 git commit -m "travis => build reports"
